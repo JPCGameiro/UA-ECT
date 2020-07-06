886624dc05389818c9397e425d6740b38d1f1fb7
JPCGameiro <joao.gameiro@ua.pt>
2020-07-06 11:21:31 +0100

    Add files via upload

--- 2ºAno/AC2/Exercicios/EP/e05/e5b/e5b.c
+++ 2ºAno/AC2/Exercicios/EP/e05/e5b/e5b.c
@@ -9,16 +9,11 @@ void delay(int ms)
     }
 }
 
-int freqToMs(int freq){
-    int ts = 1 / freq;
-    int tms = ts * 1000;
-    return tms;
-}
-
 int main(void)
 {
     printStr("João Gameiro - Nº93097\n");
     printStr("Ex5b Preparação Exame Prático - AC2\n");
+    printStr("\n");
    
     int cnt = 0;
     int freq = 10;
@@ -26,45 +21,71 @@ int main(void)
 
     while(1)
     {
-        printStr("\r");
-        printInt(cnt, 10 | 2 << 10);
-
         number = inkey();
+
+        printStr("\r");
+        if(cnt >=0 && cnt<=9)
+            printInt(0, 10);
+        printInt(cnt, 10 | 2 << 10);
 
         switch(number){
             case '0':
                 freq = 10;
-                currentNumber = freq;
+                currentNumber = '0';
+                break;
             case '1':
                 freq = 20;
-                currentNumber = freq;
+                currentNumber = '1';
+                break;         
             case '2':
                 freq = 30;
-                currentNumber = freq;
+                currentNumber = '2';
+                break;
             case '3':
                 freq = 40;
-                currentNumber = freq;
+                currentNumber = '3';
+                break;
             case '4':
                 freq = 50;
-                currentNumber = freq;
+                currentNumber = '4';
+                break;
+            case '5':
+                freq = 60;
+                currentNumber = '5';
+                break;
+            case '6':
+                freq = 70;
+                currentNumber = '6';
+                break;
+            case '7':
+                freq = 80;
+                currentNumber = '7';
+                break;
+            case '8':
+                freq = 90;
+                currentNumber = '8';
+                break;
+            case '9':
+                freq = 100;
+                currentNumber = '9';
+                break;
             case '\n':
-                printInt(cnt, 10 | 2 << 10);
                 printStr(" >> ");
+                if(cnt >=0 && cnt<=9)
+                    printInt(0, 10);
                 printInt(cnt, 10 | 2 << 10);
-                freq = 10;
-                currentNumber = 10;
+                number = currentNumber;
+                break;
             case '\0':
-                freq = currentNumber;
-            default:
-                freq = 10;
-                currentNumber = 10;
+                number = currentNumber;
+                break;
         }
 
         cnt++;
         if(cnt == 99)
             cnt = 0;
 
-        delay(freqToMs(freq));   
+        delay(1000/freq);   
     }
     return 0;
 }
