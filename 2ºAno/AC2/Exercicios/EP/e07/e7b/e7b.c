fea64a70320c7b34a9a5728241bfc230f6cd2f4b
JPCGameiro <joao.gameiro@ua.pt>
2020-07-06 11:26:15 +0100

    Add files via upload

--- 2ºAno/AC2/Exercicios/EP/e07/e7b/e7b.c
+++ 2ºAno/AC2/Exercicios/EP/e07/e7b/e7b.c
@@ -1,6 +1,15 @@
 #include <detpic32.h>
 
-//Configuração da ADC com interrupções
+volatile unsigned char voltage = 0;
+
+void delay(int ms)
+{
+    for(;ms > 0;ms--){
+        resetCoreTimer();
+        while(readCoreTimer() < 20000);
+    }
+}
+
 void configADC()
 {
     TRISBbits.TRISB4 = 1;
@@ -8,24 +17,24 @@ void configADC()
     AD1CON1bits.SSRC = 7;
     AD1CON1bits.CLRASAM = 1;
     AD1CON3bits.SAMC = 16;
-    AD1CON2bits.SMPI = 1-1; //verificar
+    AD1CON2bits.SMPI = 4-1;
     AD1CHSbits.CH0SA = 4;
     AD1CON1bits.ON = 1;
+
+    IPC6bits.AD1IP = 2;
+    IEC1bits.AD1IE = 1;
+    IFS1bits.AD1IF = 0;
 }
 
-//Configuração I/O
-void configIO()
+void _int_(27) rsi_ad1(void)
 {
-    TRISB = (TRISB & 0xFFF0) | 0x000F;
-}
-
-void delay(int ms)
-{
-    for(; ms > 0; ms--)
-    {
-        resetCoreTimer();
-        while(readCoreTimer() < 20000);
-    }    
+    int sum = 0;
+    int *p = (int *)(&ADC1BUF0);
+    for(; p <= (int *)(&ADC1BUFF);p += 4)
+        sum += *p;
+    double average = sum/4.0;
+    voltage = (char)((average*33)/1023);
+    IFS1bits.AD1IF = 0;
 }
 
 int main(void)
@@ -33,34 +42,24 @@ int main(void)
     printStr("João Gameiro - Nº93097\n");
     printStr("Ex7b Preparação Exame Prático - AC2\n");
 
+    TRISB = (TRISB & 0xFFF0) | 0x000F;
+
     configADC();
-    configIO();
-    EnableInterrupts();
+    EnableInterrupts(); 
 
     int freq = 10;
-    int t;  //verificar se é com double
-    while(1)
-    {
+    while(1){
         AD1CON1bits.ASAM = 1;
-        while(IFS1bits.AD1IF == 0);
 
-        freq = 1 + (ADC1BUF0/255);
-        t = 1/freq;
-        printStr("\nTensão: ");
-        printInt(t, 10 | 2 << 10);
-        printStr("\nFrequência: ");
-        printInt(freq, 10 | 2 << 10);
-        delay(t*1000);
-
-        IFS1bits.AD1IF = 0;
-
-        printStr("\nValor presente nos interruptores: ");
+        printStr("\nValor nos Interruptores: ");
         printInt(PORTBbits.RB0, 2);
         printInt(PORTBbits.RB1, 2);
         printInt(PORTBbits.RB2, 2);
         printInt(PORTBbits.RB3, 2);
-        printStr("\n");
+
+        freq = 1 + (voltage/255);
+        delay(1000/freq);
     }
+
     return 0;
 }
-
